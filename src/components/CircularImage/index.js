472b111e2a4b15664a4cd16e191b7827f6f9ad63
Prasanta Kakati <prasantakakati1994@gmail.com>
2022-02-13 10:05:09 +0530

    changed og and fixed lines in image container

--- src/components/CircularImage/index.js
+++ src/components/CircularImage/index.js
@@ -2,18 +2,20 @@ import React from 'react'
 import classnames from 'classnames'
 
 import s from './CircularImage.module.scss'
-const CircularImage = props => {
+const CircularImage = (props) => {
     const { className, src, alt = '', style, size = 48, ...otherProps } = props
 
     const cx = classnames(s.container, className)
     const combinedStyle = {
         width: size + 'px',
         height: size + 'px',
-        ...style
+        ...style,
     }
     return (
         <div className={cx} style={combinedStyle} {...otherProps}>
-            <img className="circular-img" alt={alt} src={src} />
+            {!!src.length && (
+                <img className="circular-img" alt={alt} src={src} />
+            )}
         </div>
     )
 }
